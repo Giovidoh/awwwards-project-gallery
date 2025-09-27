664ced16bb97f099a9bc71b46b485bb93a4f4f07
Giovidoh <giovidoh@gmail.com>
2025-09-27 00:21:22 +0000

    Update dependencies and enhance modal functionality
    - Added @gsap/react dependency for improved animation capabilities.
    - Updated package.json and pnpm-lock.yaml to include new dependencies.
    - Modified Modal component to implement mouse-follow animations and scaling effects.
    - Changed project descriptions in the Home component for consistency.
    - Adjusted Project component to correctly manage modal state on mouse events.
    - Added CSS rules to prevent body overflow during modal display.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,12 @@
+"use client";
+
 import { FC } from "react";
 import Image, { StaticImageData } from "next/image";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 interface ModalProps {
   modal: {
@@ -16,11 +23,87 @@ interface ModalProps {
 
 const Modal: FC<ModalProps> = ({ modal, projects }) => {
   const translateY = modal.index * -100 + "%";
+  const modalRef = useRef(null);
+  const viewRef = useRef(null);
+  const viewTextRef = useRef(null);
+
+  // Modal visibility
+  gsap.set(modalRef.current, { autoAlpha: 1 });
+
+  // Modal and view follow the mouse animation
+  useGSAP(() => {
+    const moveModalXTo = gsap.quickTo(modalRef.current, "left", {
+      duration: 0.8,
+      ease: "power3",
+    });
+    const moveModalYTo = gsap.quickTo(modalRef.current, "top", {
+      duration: 0.8,
+      ease: "power3",
+    });
+
+    const moveViewXTo = gsap.quickTo(viewRef.current, "left", {
+      duration: 0.4,
+      ease: "power3",
+    });
+    const moveViewYTo = gsap.quickTo(viewRef.current, "top", {
+      duration: 0.4,
+      ease: "power3",
+    });
+
+    const moveViewTextXTo = gsap.quickTo(viewTextRef.current, "left", {
+      duration: 0.5,
+      ease: "power3",
+    });
+    const moveViewTextYTo = gsap.quickTo(viewTextRef.current, "top", {
+      duration: 0.5,
+      ease: "power3",
+    });
+
+    window.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+
+      moveModalXTo(clientX);
+      moveModalYTo(clientY);
+
+      moveViewXTo(clientX);
+      moveViewYTo(clientY);
+
+      moveViewTextXTo(clientX);
+      moveViewTextYTo(clientY);
+    });
+  });
+
+  // Modal Scale animation
+  useGSAP(() => {
+    if (modal.active) {
+      gsap.to(modalRef.current, {
+        scale: 1,
+        duration: 0.4,
+        ease: "power3",
+      });
+    } else {
+      gsap.to(modalRef.current, {
+        scale: 0,
+        duration: 0.4,
+        ease: "power3",
+      });
+    }
+  }, [modal.active]);
 
   return (
-    <div className="absolute flex justify-center items-center w-fit h-[250px] overflow-hidden left-1/2 top-0 -translate-x-1/2">
+    <div
+      ref={modalRef}
+      className="absolute flex justify-center items-center w-fit h-[250px] overflow-hidden pointer-events-none invisible"
+    >
       <div
-        className="flex flex-col w-full h-full transition-all duration-500 ease-[cubic-bezier(0.76,0,0.24,1)]"
+        ref={viewRef}
+        className="fixed top-0 left-0 bg-blue-500 text-white rounded-full h-20 w-20 flex items-center justify-center z-10"
+      >
+        <span ref={viewTextRef}>View</span>
+      </div>
+      <div
+        className="flex flex-col w-full h-full transition-all duration-600 ease-[cubic-bezier(0.76,0,0.24,1)]"
+        // Animation of the slide in the modal
         style={{ transform: `translateY(${translateY})` }}
       >
         {projects.map((project, index) => {
